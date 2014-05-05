dcfa98c1aadeebd32e28436c73b9cde2ff937b95
Dean Eigenmann <dean.eigenmann@icloud.com>
2014-05-05 20:35:32 +0200

    Update ui.js
    No clue if this works, but a proposed pdf generator

--- js/ui.js
+++ js/ui.js
@@ -297,6 +297,23 @@ var ui = (function() {
 				text = header + body;
 				text = text.replace(/\t/g, '');
 			break;
+			
+			case 'pdf':
+				function printPDF(){
+    				var printDoc = new jsPDF();
+
+
+   				};
+				
+			
+			    printDoc.fromHTML($('#pdf').get(0), 10, 10, {
+			        'width': 180
+			    });
+			
+			    printDoc.autoPrint()
+			    printDoc.output("dataurlnewwindow") // this opens a new popup,  after this the PDF opens the print window view but there are browser inconsistencies with how this is handled
+}
+			break;
 
 			case 'markdown':
 				header = header.replace(/\t/g, '');
