8224ab0c591c7db713c041d78344d2baea4f84c9
Knut Ole <knutolesjoli@gmail.com>
2014-09-23 23:45:41 +0200

    Update utils.js
    Global ZenPen and refactor of trim() away from global space.

--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,35 @@
 // Utility functions
+ZenPen = window.ZenPen || {};
+ZenPen.util = {
 
-String.prototype.trim = function(){ return this.replace(/^\s+|\s+$/g, ''); };
+	supportsHtmlStorage : function () {
+		try {
+		    return 'localStorage' in window && window['localStorage'] !== null;
+		} catch (e) {
+		    return false;
+		}
+	},
 
-function supportsHtmlStorage() {
-	try {
-		return 'localStorage' in window && window['localStorage'] !== null;
-	} catch (e) {
-		return false;
+	get_text : function (el) {
+		var ret = " ";
+		var length = el.childNodes.length;
+		for(var i = 0; i < length; i++) {
+		    var node = el.childNodes[i];
+		    if(node.nodeType != 8) {
+
+			if ( node.nodeType != 1 ) {
+			    // Strip white space.
+			    ret += node.nodeValue;
+			} else {
+			    ret += get_text( node );
+			}
+		    }
+		}
+		return ZenPen.util.trim(ret);
+	},
+
+	trim : function(string) { 
+		return string.replace(/^\s+|\s+$/g, ''); 
 	}
-}
 
-function get_text(el) {
-    var ret = " ";
-    var length = el.childNodes.length;
-    for(var i = 0; i < length; i++) {
-        var node = el.childNodes[i];
-        if(node.nodeType != 8) {
-
-        	if ( node.nodeType != 1 ) {
-        		// Strip white space.
-        		ret += node.nodeValue;
-        	} else {
-        		ret += get_text( node );
-        	}
-        }
-    }
-    return ret.trim();
 }
